913855b24895248d761510ce8e0c44633aa9fe45
dac0n <dac0noid@gmail.com>
2021-06-07 02:52:04 +0300

    task_finished

--- src/Triangle.js
+++ src/Triangle.js
@@ -1,6 +1,23 @@
 import React, { useEffect, useState } from 'react';
 
-const getBase = (sides) => {
+
+//HELPER_FUNCTIONS
+/* these functions are abstracted from main code
+to increase readability and make things easier
+to maintain and debug */
+
+const isEquilateral = (sides) => {
+  return sides[0] == sides[1] && sides[0] == sides[2];
+}
+
+const isScalene = (sides) => {
+  return sides[0] !== sides[1] && sides[0] !== sides[2] && sides[1] !== sides[2];
+}
+
+
+
+const getBase = (sides) => {    
+  //returns the side which will be the lowest (and from which height of a triangle is calculated)
   let uniqueAmount = (new Set(sides)).size;
   let base = uniqueAmount !== 2 ? Math.max(...sides) : sides.find((x) => { return sides.indexOf(x) === sides.lastIndexOf(x); });
   return base;
@@ -14,31 +31,54 @@ const getHeight = (sides, base) => {
 }
 
 const getBorders = (sides, base, height) => {
+  //in this project triangle is drawn by using css borders
+  // the apex of the triangle perpendicular to the base is located between two borders
   let borders = [];
   for (let side of sides) {
-    if (side !== base || (sides[0] == sides[1] && sides[0] == sides[2] && borders.length < 2)) borders.push(Math.sqrt(side ** 2 - height ** 2));
+    if (side !== base || ( isEquilateral(sides) && borders.length < 2)) borders.push(Math.sqrt(side ** 2 - height ** 2));
   }
   return borders;
 }
 
 const updateDrawing = (borders, height) => {
+  // as canvas was not used, this is the way to implement dynamic styling
   root.style.setProperty("--border_bottom_width", `${height}px`);
   root.style.setProperty("--left_border", `${borders[0]}px`);
   root.style.setProperty("--right_border", `${borders[1]}px`);
 }
 
+const setMessage = (type, text) => {
+  const elId = type == 'info' ? 'triangle-info' : 'triangle-placeholder';
+  document.getElementById(elId).innerHTML = text;
+}
+
+const updateInfo = (sides) => {
+  let triangleInfo = isEquilateral(sides) ? 'Equilateral Triangle' : isScalene(sides) ? 'Scalene Triangle' : 'Isosceles Triangle';
+  setMessage('info', triangleInfo);
+}
+
+const toggleVisibility = (type) => {  //we need this to display messages and triangle correctly
+  document.getElementById('triangle-placeholder').style.display = type == 'message' ? 'block' : 'none';
+  document.getElementsByClassName('triangle-child')[0].style.display = type == 'triangle' ? 'block' : 'none';
+}
 
 
+////////////////////////////////////////
 
 const Triangle = (props) => {
 
   const PIXELS_PER_CM = 37.7952755906;    //a constant taken from wikipedia
-  const MAXIMAL_DRAWING_SIZE = Math.max(props.height.slice(0, -2), props.width.slice(0, -2)) / PIXELS_PER_CM;
+  console.log(PIXELS_PER_CM);
+  const MAXIMAL_DRAWING_SIZE = Math.min(props.height.slice(0, -2), props.width.slice(0, -2)) / PIXELS_PER_CM;
 
   const [sidesInCm, setSidesInCm] = useState([4, 4, 4]);
-  root.style.setProperty("--component-height", `${props.height}`);
-  root.style.setProperty("--component-width", `${props.width}`);
+  const [tempSides, setTempSides] = useState([4, 4, 4]);
 
+
+  root.style.setProperty("--component-height", `${props.height}`);
+  //allows us to set component sizes when calling the component,
+  //without changing css styles
+  root.style.setProperty("--component-width", `${props.width}`);
 
 
   useEffect(() => {
@@ -48,27 +88,44 @@ const Triangle = (props) => {
     let sidesInPixels = sidesInCm.map(x => x * PIXELS_PER_CM * zoom);
     //if biggest side if bigger than canvas, it is resized to fit the canvas. 
     //otherwise zoom is 1 and size is not changed (and pixel-cm ratio is untouched)
-
-    console.log('useEffect sides:', sidesInCm);
     let base = getBase(sidesInPixels);
     let height = getHeight(sidesInPixels, base);
     let borders = getBorders(sidesInPixels, base, height)
+
+    if ([...borders, height].some(el => isNaN(el))) {
+      toggleVisibility('message');
+      setMessage('info', '');
+      setMessage('error', 'Triangle does not exist.')
+    }
+    else{
+      toggleVisibility('triangle');
+      updateInfo(sidesInCm);
+    }
+
     updateDrawing(borders, height);
-    console.log(`borders: ${borders}, Height: ${height}, leftborder: ${borders[0]}, rightborder: ${borders[1]}`)
   }, [sidesInCm])
 
 
 
 
-
-
   const changeHandler = (e, number) => {
-    let tempSides = [...sidesInCm]; //we need this to force React into re-rendering after setSides.
-    //otherwise it will not rerender, since it will be the same object.
-    tempSides[number] = parseInt(e.target.value) || 0;
-    console.log('Temp sides after change', tempSides)
-    setSidesInCm(tempSides);
+    console.log(tempSides);
+    let __tempSides = [...tempSides]; //needed to force React to rerender
+    __tempSides[number] = parseInt(e.target.value) || 0;
+    setTempSides(__tempSides);
 
+  }
+
+  const submitHandler = () => {
+
+    if (tempSides.some(e => e == 0)) {
+      toggleVisibility('message');
+      setMessage('info', '');
+      setMessage('error', 'Please enter all the values!');
+    }
+    else {
+      setSidesInCm(tempSides);
+    }
   }
 
   return (
@@ -76,12 +133,15 @@ const Triangle = (props) => {
       <div className="triangle-container">
         <div className="triangle-child" />
       </div>
+      <div className="triangle-text" id="triangle-placeholder" />
+      <div className="triangle-text" id="triangle-info" />
       <div className="triangle-inputs">
         <div>
           <label>Side A:</label>
           <input
             type="number"
             onChange={(e) => { changeHandler(e, 0) }}
+            defaultValue="4"
           />
         </div>
         <div>
@@ -89,6 +149,7 @@ const Triangle = (props) => {
           <input
             type="number"
             onChange={(e) => { changeHandler(e, 1) }}
+            defaultValue="4"
           />
         </div>
         <div>
@@ -96,8 +157,10 @@ const Triangle = (props) => {
           <input
             type="number"
             onChange={(e) => { changeHandler(e, 2) }}
+            defaultValue="4"
           />
         </div>
+        <button id="triangle-calculate-btn" onClick={() => { submitHandler() }}>Calculate</button>
       </div>
     </div>
   );
